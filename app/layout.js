ff2535ac2c9559717ccbd80da2714c6281e96431
subodh <subodhpokrel10@gmail.com>
2024-02-09 20:22:31 +0545

    .

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
-import { Inter } from "next/font/google";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -12,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>{children}</body>
+      <body className={montserrat.className}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
